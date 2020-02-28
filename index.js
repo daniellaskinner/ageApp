f190bcfa332fa84a570318e9285a04182e390c04
daniellaskinner <daniellaskinner120@gmail.com>
2020-02-28 14:13:47 +0000

    added functionality to grab name and age data from API and output on screen

--- index.js
+++ index.js
@@ -2,55 +2,41 @@
 let nameSpan = document.getElementById("nameSpan")
 let ageSpan = document.getElementById("ageSpan")
 
-//grab the value inputted in field
+//grab the value inputted in field MIGHT NOT NEED THIS BIT
 let userName = []
 let userNameInput = document.getElementById("typedName")
 
 //function to insert name onto screen
-function insertNameToSpan(){
-    userName.push(userNameInput.value)
-    clearAndShow()
+function insertNameInfoToSpan(nameData){
+    //insert what comes back from API into html elements (returned as an object so grab relevant keys)
+    nameSpan.innerHTML = nameData.name
+    ageSpan.innerHTML = nameData.age
 }
 
-//function to clear the name array and display on span
-function clearAndShow(){
-    nameSpan.innerHTML=`${userName}`
-    userName.pop()
-}
-
-//need event listener to prevent default of form submit button, call the insertname to span function on submit
-document.getElementById("submit").addEventListener("click", function(e){
+//need event listener to prevent default of form submit button, call the insertName function to span function on submit
+document.getElementById("submit").addEventListener("click", async function(e){
     e.preventDefault()
-    insertNameToSpan()
+    let nameData = await getApiData()
+    insertNameInfoToSpan(nameData)
 })
 
-//do the above on click event (pass functions into here
-// document.getElementById("submit").addEventListener("click", function(e){
-//     //chuck users name into span
-//     nameSpan.innerHTML=`${usersName}`
-// })
-
-
 // fetch name and age that comes back from API
-let stringToSend =  'https://api.agify.io?name=' + userName
-
 async function getApiData() {
-    let ageData = await fetch(stringToSend)
+    let nameStringToSend = document.getElementById("typedName").value
+    let ageData = await fetch('https://api.agify.io?name=' + nameStringToSend)
     // let nationalityData = fetch('')
-    let ageDataResult = ageData.json()
+    let ageDataResult = await ageData.json()
     // let nationalityDataResult = await nationalityData.json()
-    console.log(ageDataResult)
+    return ageDataResult
 }
 
-getApiData()
-
-// let person = {}
-
-
-
-//insert what comes back from API into html elements (returned as an object so grab relevant keys)
-
+async function logApiData(){
+    let nameToScreen = await getApiData()
+    console.log(nameToScreen)
+}
 
 //pass name into nationalize function
 //define functions outside the the async and then call them inside the event listener
 //async the event listener 'e' function too to make it work
+
+//Add chart.js
